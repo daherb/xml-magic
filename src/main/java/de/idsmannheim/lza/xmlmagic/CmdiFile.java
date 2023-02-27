7e3f931984fea16fddeec9479436951306575ecd
Herbert Lange <lange@ids-mannheim.de>
2023-02-27 15:01:34 +0100

    add handy functions for cmdi

--- src/main/java/de/idsmannheim/lza/xmlmagic/CmdiFile.java
+++ src/main/java/de/idsmannheim/lza/xmlmagic/CmdiFile.java
@@ -4,14 +4,23 @@
  */
 package de.idsmannheim.lza.xmlmagic;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.jdom2.Document;
 import org.jdom2.Element;
+import org.jdom2.JDOMException;
 import org.jdom2.Namespace;
 import org.jdom2.filter.ElementFilter;
+import org.jdom2.input.SAXBuilder;
 import org.jdom2.xpath.XPathFactory;
 
 /**
@@ -46,4 +55,46 @@ public class CmdiFile extends XmlFile {
         }
         return componentElements;
     }
+    
+    public Map<String, String> getCmdiProfiles() {
+        HashMap<String,String> profiles = new HashMap<>();
+        List<String> uris = new ArrayList<>(getRootSchemas());
+        uris.addAll(getRootNamespaces().values());
+        for (String uri: uris) {
+            if (uri.matches("http(s)?://catalog.clarin.eu/ds/ComponentRegistry/.*")) {
+                // Try to downlaod the profile specification (not the schema -> remove potentially trailing /xsd)
+                try {
+                    URL url = new URL(Pattern.compile("/xsd$").matcher(uri).replaceAll("/xml"));
+                    LOG.info(url.toString());
+                    Document profile = new SAXBuilder().build(url);
+                    List<Element> headers = XPathFactory.instance().compile("//Header", new ElementFilter())
+                                .evaluate(profile);
+                    for (Element e : headers) {
+                        profiles.put(e.getChild("ID").getText(), e.getChild("Name").getText());
+                    }
+                }
+                catch (IOException | JDOMException e) {
+                    
+                }
+            }
+        }
+        return profiles;
+    }
+
+    @Override
+    public boolean matches() {
+        return isCmdiFile();
+    }
+
+    @Override
+    public MimeType getMimeType() {
+        HashMap<String,String> parameters = new HashMap<>();
+        for (String profile : getCmdiProfiles().keySet()) {
+            parameters.put("profile", profile);
+        }
+        return new MimeType("application","x-cmdi","xml").addParameters(parameters);
+    }
+    
+    
+    private static final Logger LOG = Logger.getLogger(CmdiFile.class.getName());
 }
