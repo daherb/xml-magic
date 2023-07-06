a2b93952e23141d0fc8b0eeca96a4bdc3c59be09
Herbert Lange <lange@ids-mannheim.de>
2023-07-06 12:16:02 +0200

    add compile command

--- install-pom.sh
+++ install-pom.sh
@@ -1,1 +1,2 @@
+mvn clean compile package install
 mvn install:install-file -Dfile=target/xml-magic-1.0-SNAPSHOT.jar -DgroupId=de.ids-mannheim.lza -DartifactId=xml-magic -Dversion=1.0 -Dpackaging=jar  -DgeneratePom=true
